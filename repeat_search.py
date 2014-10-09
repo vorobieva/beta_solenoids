1a7e4db6a86ce701ad6ffb0a86b1154393b5afdb
Harley Pyles <pylesharley@gmail.com>
2014-10-09 14:13:56 -0700

    muting excessive rosetta commments

--- repeat_search.py
+++ repeat_search.py
@@ -13,7 +13,7 @@ import sys
 import os
 
 import rosetta
-rosetta.init()
+rosetta.init(extra_options = "-mute basic -mute core -mute protocols")
 
 ThreeToOne = {'GLY':'G','ALA':'A','VAL':'V','LEU':'L','ILE':'I','MET':'M','PRO':'P','PHE':'F','TRP':'W','SER':'S','THR':'T','ASN':'N','GLN':'Q','TYR':'Y','CYS':'C','CYD':'C','LYS':'K','ARG':'R','HIS':'H','ASP':'D','GLU':'E','STO':'*','UNK':'U'}
 ChainAlphabetIndices = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26 }
