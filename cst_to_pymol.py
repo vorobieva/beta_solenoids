1047514da86eb66bc11598ba1e42613e9cb69dbb
Harley <pylesharley@gmail.com>
2014-11-25 18:52:32 -0800

    multi function added to cst_to_pymol

--- cst_to_pymol.py
+++ cst_to_pymol.py
@@ -24,30 +24,43 @@ def main(argv = None):
     argv = sys.argv
   
   ArgParser = argparse.ArgumentParser(description=' cst_to_pymol.py -cst contstraint_file.cst ')
-  ArgParser.add_argument('-cst', type=str, help=' input cst file ', required=True)
+  ArgParser.add_argument('-cst', type=str, nargs='+', help=' input cst file ', required = True)
+  ArgParser.add_argument('-multi', type=bool, help=' for viewing multiple cst in pymol ', default = False)
   Args = ArgParser.parse_args()
 
-  with open(Args.cst, 'r') as CstInput:
-    CstLines = CstInput.readlines()
+  for cst in Args.cst:
+    with open(cst, 'r') as CstInput:
+      CstLines = CstInput.readlines()
+    if Args.multi:
+      NameTag = (cst+'!').replace('.cst!', '').replace('!', '')
 
-  for Index, Line in enumerate(CstLines):
-    if Line.startswith('AtomPair'):
-      CstType, Name1, Res1, Name2, Res2 = tuple( Line.split()[:5] )
-      print 'dist %s, name %s and resi %s, name %s and resi %s'%('cst_%d_dist'%(Index+1), Name1, Res1, Name2, Res2)
-    
-    elif Line.startswith('Angle'):
-      CstType, Name1, Res1, Name2, Res2, Name3, Res3 = tuple( Line.split()[:7] )
-      print 'angle %s, name %s and resi %s, name %s and resi %s, name %s and resi %s'%('cst_%d_angle'%(Index+1), Name1, Res1, Name2, Res2, Name3, Res3)
+    for Index, Line in enumerate(CstLines):
+      if Line.startswith('AtomPair'):
+        CstType, Name1, Res1, Name2, Res2 = tuple( Line.split()[:5] )
+        if Args.multi:
+          print 'dist %s, name %s and resi %s, name %s and resi %s'%('cst_%d_dist'%(Index+1), Name1, Res1, Name2, Res2)
+        else:
+          print 'dist %s, name %s and resi %s and %s, name %s and resi %s and %s'%('cst_%d_dist'%(Index+1), Name1, Res1, NameTag, Name2, Res2, NameTag)
 
-    elif Line.startswith('Dihedral'):
-      CstType, Name1, Res1, Name2, Res2, Name3, Res3, Name4, Res4 = tuple( Line.split()[:9] )
-      print 'dihedral %s, name %s and resi %s, name %s and resi %s, name %s and resi %s, name %s and resi %s'%('cst_%d_tor'%(Index+1), Name1, Res1, Name2, Res2, Name3, Res3, Name4, Res4)
+      elif Line.startswith('Angle'):
+        CstType, Name1, Res1, Name2, Res2, Name3, Res3 = tuple( Line.split()[:7] )
+        if Args.multi:
+          print 'angle %s, name %s and resi %s, name %s and resi %s, name %s and resi %s'%('cst_%d_angle'%(Index+1), Name1, Res1, Name2, Res2, Name3, Res3)
+        else:
+          print 'angle %s, name %s and resi %s and %s, name %s and resi %s and %s, name %s and resi %s and %s'%('cst_%d_angle'%(Index+1), Name1, Res1, NameTag, Name2, Res2, NameTag, Name3, Res3, NameTag)
 
-  print 'set dash_width, 1'
-  print 'color red, cst*angle'
-  print 'set dash_width, 2, cst*angle'
-  print 'color cyan, cst*dist'
-  print 'set dash_width, 5, cst*dist'
+      elif Line.startswith('Dihedral'):
+        CstType, Name1, Res1, Name2, Res2, Name3, Res3, Name4, Res4 = tuple( Line.split()[:9] )
+        if Args.multi:
+          print 'dihedral %s, name %s and resi %s, name %s and resi %s, name %s and resi %s, name %s and resi %s'%('cst_%d_tor'%(Index+1), Name1, Res1, Name2, Res2, Name3, Res3, Name4, Res4)
+        else:
+          print 'dihedral %s, name %s and resi %s and %s, name %s and resi %s and %s, name %s and resi %s, and %s name %s and resi %s and %s'%('cst_%d_tor'%(Index+1), Name1, Res1, NameTag, Name2, Res2, NameTag, Name3, Res3, NameTag, Name4, Res4, NameTag)
+
+    print 'set dash_width, 1'
+    print 'color red, cst*angle'
+    print 'set dash_width, 2, cst*angle'
+    print 'color cyan, cst*dist'
+    print 'set dash_width, 5, cst*dist'
 
 if __name__ == "__main__":
   sys.exit(main())
